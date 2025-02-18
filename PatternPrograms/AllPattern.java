2e425ecf8d2d25c7ca2e9eba528b0cfbdc10e725
Aniket Sanjay Suryavanshi <99653627+AniketSuryavanshi3@users.noreply.github.com>
2025-02-18 11:11:40 +0530

    covered all StarPatterns

---
+++ PatternPrograms/AllPattern.java
@@ -0,0 +1,160 @@
+import java.util.Scanner;
+class AllPattern 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = 5;
+		
+		for (int i=1;i<=n ;i++ )        /*A*/
+		{
+		//	for (int j=1;j<=n-i;j++ )
+		//	{
+		//		System.out.print("  ");
+		//	}
+			for (int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		for (int i=1;i<=n ;i++ )			/*B*/
+		{
+			for (int j=1;j<=n-i;j++ )
+			{
+				System.out.print("  ");
+			}
+			for (int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		System.out.println("");
+		
+		for (int i=n;i>=1 ;i-- )			/*C*/
+		{
+		//	for (int j=1;j<=n-i;j++ )
+		//	{
+		//		System.out.print("  ");
+		//	}
+			for (int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		for (int i=n;i>=1 ;i-- )				/*D*/
+		{
+			for (int j=1;j<=n-i;j++ )
+			{
+				System.out.print("  ");
+			}
+			for (int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		for (int i=0;i<=n ;i++ )        /*AB*/
+		{
+		//	for (int j=1;j<=n-i;j++ )
+		//	{
+		//		System.out.print("  ");
+		//	}									
+			for (int j=1;j<=n-i;j++ )
+			{
+				System.out.print("  ");
+			}
+			for (int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			for (int j=2;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		System.out.println();										
+
+		for (int i=n;i>=1 ;i-- )			/*CD*/
+		{
+			for (int j=1;j<=n-i;j++ )
+			{
+				System.out.print("  ");
+			}
+			for (int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+		//	for (int j=1;j<=n-i;j++ )
+		//	{
+		//		System.out.print("  ");
+		//	}
+			for (int j=2;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		for (int i=1;i<=n ;i++ )        /*AC*/
+		{
+		//	for (int j=1;j<=n-i;j++ )
+		//	{
+		//		System.out.print("  ");
+		//	}
+			for (int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		for (int i=n;i>=1 ;i-- )			
+		{
+		//	for (int j=1;j<=n-i;j++ )
+		//	{
+		//		System.out.print("  ");
+		//	}
+			for (int j=2;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+													/*BD*/
+		
+		for (int i=1;i<n ;i++ )			/*B*/
+		{
+			for (int j=1;j<=n-i;j++ )
+			{
+				System.out.print("  ");
+			}
+			for (int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		for (int i=n;i>=1 ;i-- )				/*D*/
+		{
+			for (int j=1;j<=n-i;j++ )
+			{
+				System.out.print("  ");
+			}
+			for (int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
